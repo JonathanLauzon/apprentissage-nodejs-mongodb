fa6adb0d7dec299013334c54b1bedae5a9e44833
Jonathan Lauzon <jonathan.lauzonv4si@gmail.com>
2017-03-02 11:14:01 -0500

    Finalisation de l'exercice et changement de la stratégie de base de données en local.

--- exercice_09.js
+++ exercice_09.js
@@ -4,34 +4,41 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 var db;
 
-MongoClient.connect('mongodb://jonathan-lauzon:e1206261@ds053764.mlab.com:53764/carnet_adresses', (err, database) => {
+MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresses', (err, database) => {
 	if (err) return console.log(err)
 	db = database
 	app.listen(8081, () => {
 		console.log('connexion à la BD et on écoute sur le port 8081');
 	})
-}
+})
+
+app.get('/',  (req, res) => {
+  console.log('la route route get / = ' + req.url)
+  var cursor = db.collection('adresses').find().toArray(function(err, resultat){
+    if (err) return console.log(err);
+    // renders index.ejs
+    // affiche le contenu de la BD
+    res.render('index.ejs', {liste: resultat});
+  })
+})
 
 
-app.post('/ajouter', urlencodedParser, function (req, res) {
+app.post('/ajouter', function (req, res) {
 
 	nouvelUtilisateur = {
 		nom:req.body.nom,
 		prenom:req.body.prenom,
 		telephone:req.body.telephone,
 		ville:req.body.ville,
-		codepostal:req.body.codepostal,
-		lamethode: "POST"
+		codepostal:req.body.codepostal
 	};
 	console.log('reponse');
 
-	var lUtilisateur = JSON.stringify(nouvelUtilisateur);
-
-	db.collection('adresses').save(lUtilisateur, (err, result) => {
+	db.collection('adresses').save(nouvelUtilisateur, (err, result) => {
 		if (err) return console.log(err);
 		console.log('sauvegarder dans la BD');
 		res.redirect('/');
